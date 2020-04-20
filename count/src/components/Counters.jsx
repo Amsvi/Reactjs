2d34e1362d77c9302352b12fc08646816e6d1bd2
MACOFA <macofa@MACOFAs-MacBook-Pro.local>
2020-04-20 05:58:25 +0430

    add single Source of truth, remove local state

--- count/src/components/Counters.jsx
+++ count/src/components/Counters.jsx
@@ -4,12 +4,29 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 0, value: 2 },
-      { id: 1, value: 9 },
-      { id: 2, value: 1 },
-      { id: 3, value: 6 },
+      { id: 0, value: 4 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
     ],
   };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   //we dont update the state directly, we should make a array without given deleteID
   //and then call setstate of component and let the react update the state
   handleDeleteEvent = (deleteID) => {
@@ -19,11 +36,18 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
-            onDeleteEvent={this.handleDeleteEvent}
+            onDelete={this.handleDeleteEvent}
             key={counter.id}
             counter={counter}
+            onIncrement={this.handleIncrement}
           ></Counter>
         ))}
       </div>
--- count/src/components/counter.jsx
+++ count/src/components/counter.jsx
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-  // recieve argument to this function
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-  buttomRnd() {
-    let butooo = "btn m-2 badge-";
-    butooo += this.state.value === 0 ? "warning" : "success";
-    return butooo;
-  }
+  // by removing state it becomes controlled component
+  // it recieves all the data via props and raises event when ever data need to be changed.
+  // at this time this component is entirely controlled by its parent.
   render() {
     return (
       <div>
-        {/* in this case we could use props simply as I changed 
-        the code we get same result children is use for times
-         there are complex element to a child component such as 
-         dialog bux in those case we use children */}
-        <h4>Counter # {this.props.id}</h4>
-        <button className={this.buttomRnd()}>{this.state.value}</button>
+        <span className={this.buttomRnd()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn m-2 badge-primary"
         >
           increment
@@ -31,13 +17,23 @@ class Counter extends Component {
         <button
           //here counter componenet is raising an event
           // its parent (counterS) component is handling that event
-          onClick={() => this.props.onDeleteEvent(this.props.counter.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
         </button>
       </div>
     );
+  }
+  buttomRnd() {
+    let butooo = "badge m-2 badge-";
+    butooo += this.props.counter.value === 0 ? "warning" : "success";
+    return butooo;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
   }
 }
 
